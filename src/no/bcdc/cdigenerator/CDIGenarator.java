2f0fa2a1d637df9b08e4e2a6c0cc3d82105206f6
Steve Jones <steve@squaregoldfish.co.uk>
2016-06-29 11:10:20 +0200

    Stub program - project setup

---
+++ src/no/bcdc/cdigenerator/CDIGenarator.java
@@ -0,0 +1,10 @@
+package no.bcdc.cdigenerator;
+
+public class CDIGenarator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I am the CDI Generator");
+	}
+
+}
