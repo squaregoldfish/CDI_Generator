a4af34ba08ad9f188d22b0ef8fa47ad72163dec9
Steve Jones <steve@squaregoldfish.co.uk>
2016-07-12 16:21:00 +0200

    Javadoc

--- src/no/bcdc/cdigenerator/ConfigException.java
+++ src/no/bcdc/cdigenerator/ConfigException.java
@@ -1,11 +1,30 @@
 package no.bcdc.cdigenerator;
 
+/**
+ * Exception for the CDI Generator configuration
+ * @author Steve Jones
+ *
+ */
 public class ConfigException extends Exception {
+	
+	/**
+	 * Serial Version UID
+	 */
+	private static final long serialVersionUID = 8613470085736098536L;
 
+	/**
+	 * Simple constructor with just a message
+	 * @param message The message
+	 */
 	public ConfigException(String message) {
 		super(message);
 	}
 	
+	/**
+	 * Simple constructor with a message and a cause
+	 * @param message The message
+	 * @param cause The underlying cause
+	 */
 	public ConfigException(String message, Throwable cause) {
 		super(message, cause);
 	}
